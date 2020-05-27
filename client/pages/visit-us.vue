7505148191778f7a7c50295f2390a96f13fc8f34
Justin Chiou <chiou.kai@gmail.com>
2020-05-27 18:42:12 -0400

    finish page for /visit-us

--- client/pages/visit-us.vue
+++ client/pages/visit-us.vue
@@ -1,6 +1,12 @@
 <template>
-  <section class="container">
-    <h1 class="text-center pb-3">Visit Us!</h1>
-    <article></article>
+  <section class="container text-center">
+    <h1 class="pb-3">Visit Us!</h1>
+    <article class="mb-4">
+      <img
+        :src="require('@/assets/map.jpg')"
+        alt="map.jpg"
+        class="img-fluid img-thumbnail"
+      />
+    </article>
   </section>
 </template>
