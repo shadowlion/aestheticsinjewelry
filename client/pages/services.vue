389f229e07d8144526cc3f7e726117f3abb06571
Justin Chiou <chiou.kai@gmail.com>
2020-05-27 19:14:01 -0400

    init services page

--- client/pages/services.vue
+++ client/pages/services.vue
@@ -1,6 +1,220 @@
 <template>
   <section class="container">
     <h1 class="text-center pb-3">Services Provided</h1>
-    <article></article>
+    <article class="pb-4">
+      <b-card no-body>
+        <b-tabs card pills fill>
+          <b-tab title="Repairs (w/lasers)" active>
+            <b-card-title>Ring Resizing</b-card-title>
+            <b-card-text>
+              We adjust the size of your ring to fit properly. Even eternity
+              bands with stones all of the way around can be sized. For
+              customers with enlarged knuckles or those with fluctuating
+              swelling problems we offer a number of solutions for a proper
+              comfortable fit.
+            </b-card-text>
+
+            <b-card-title>Prong Retipping</b-card-title>
+            <b-card-text>
+              If you are snagging clothing, have a bent prong or loose stone we
+              can advise you during your semi-annual checkup of the necessary
+              action to fix it. It could involve tightening a stone, retipping,
+              repronging or replacing a head.
+            </b-card-text>
+
+            <b-card-title>Chain & Bracelet Repair</b-card-title>
+            <b-card-text>
+              We size bangles, up or down. Remove dents, engraving and
+              scratches. Adjust, repair or replace catches, hinges or safeties.
+              We can also, engrave, overlay, pierce designs or pave with stones.
+            </b-card-text>
+            <b-card-title>Eye Glass Repair</b-card-title>
+            <b-card-text>We do laser welding of metal rim glasses.</b-card-text>
+            <b-card-title>Silver Flatware Repair</b-card-title>
+            <b-card-text>
+              If your flatware was chewed up in garbage disposal or broken,
+              loose or separated from its handle, we can repair it like new.
+            </b-card-text>
+            <b-card-title>Silver Holloware Repair</b-card-title>
+            <b-card-text>
+              If your hollowware has missing parts we can duplicate them. We
+              also remove dents and engraving. We can add new insulators on
+              coffee, tea or chocolate pot handles installed with exotic wood or
+              ivory.
+            </b-card-text>
+            <b-card-title>Costume Jewelry Repair</b-card-title>
+            <b-card-text>
+              We can do all types of repairs and stone replacements for costume
+              jewelry. Each piece will need to be reviewed to determine the
+              options and pricing.
+            </b-card-text>
+            <b-card-title>Necklaces or Bracelets</b-card-title>
+            <b-card-text>
+              We can repair Link, Hinged, Mesh and Stone pieces. Lengthen and
+              shorten or make adjustable. We also can reinforce, repair or
+              replace links, catches and hinges and install safety catches.
+            </b-card-text>
+            <b-card-title>Semi-Annual Inspection</b-card-title>
+            <b-card-text>
+              We recommend periodic cleaning and checking all of your jewelry to
+              keep it looking good, prevent losing stones and to advise you of
+              any repairs needed. If you notice any irregularities, like
+              catching in clothing, broken or missing stones, or bent shank,
+              then it's time to visit Aesthetics In Jewelry.
+            </b-card-text>
+          </b-tab>
+          <b-tab title="Repairs (non-laser)">
+            <b-card-title>Pearl or Bead Restringing</b-card-title>
+
+            <b-card-text>
+              We can match, lengthen, shorten or redesign. We can replace a
+              clasp or repair clasps. We also clean and offer Pearl repair,
+              replacement and matching.
+            </b-card-text>
+
+            <b-card-text>
+              Knotted on silk, color thread or tiger tail. We also do complex
+              pattern and seed pearl stringing.
+            </b-card-text>
+
+            <b-card-title>Retreat Stones</b-card-title>
+
+            <b-card-text>
+              Many precious stones are treated to improve appearance by oiling,
+              waxing or Excel treatment. Waxing and oiling periodically will
+              need to be redone as it is removed during normal cleaning
+              procedures. Excel is a permanent treatment and jewelry can be
+              cleaned without disturbing the process, however it can be removed
+              if desired.
+            </b-card-text>
+
+            <b-card-title>Recut Stones</b-card-title>
+
+            <b-card-text>
+              There is nothing worse than a broken or damaged stone. Damaged
+              stones should be brought in for assessment so we can best advise
+              you which direction to take. This would be a choice repair, total
+              recut or reshaping.
+            </b-card-text>
+
+            <b-card-title>Repolish Stones</b-card-title>
+
+            <b-card-text>
+              That are scratched, abraided or chemically damaged.
+            </b-card-text>
+
+            <b-card-title>Polish Jewelry</b-card-title>
+
+            <b-card-text>
+              We can lightly polish or completely refinish.
+            </b-card-text>
+
+            <b-card-title>Polish Silverware</b-card-title>
+
+            <b-card-text>
+              We can highlight, lightly polish or completely refinish.
+            </b-card-text>
+
+            <b-card-title>Enameling</b-card-title>
+
+            <b-card-text>
+              We offer Champleve and Cloisonne repair. We use UV light activated
+              enamels that are used by dentists. We can either repair or replace
+              the enamel.
+            </b-card-text>
+
+            <b-card-title>Stonecutting</b-card-title>
+
+            <b-card-text>
+              We can repolish, recut and treat stones. We work with both
+              cabochon and faceted stones. We also offer stone engraving, cameo
+              repair and inlay replacement and cameo repairs including nose
+              jobs.
+            </b-card-text>
+
+            <b-card-title>Semi-Annual Inspection</b-card-title>
+
+            <b-card-text>
+              We recommend periodic cleaning and checking all of your jewelry to
+              keep it looking good, prevent losing stones and to advise you of
+              any repairs needed. If you notice any irregularities, like
+              catching in clothing, broken or missing stones, or bent shank,
+              then it's time to visit Aesthetics In Jewelry.
+            </b-card-text>
+          </b-tab>
+          <b-tab title="Custom Designs/Engravings">
+            <b-card-title>Custom Design Services</b-card-title>
+            <b-card-text>
+              <ul>
+                <li>Computer Generated (CAD Cam, Milling and 3D Printing)</li>
+                <li>Modification Services (existing designs)</li>
+                <li>Casting (Gold, Silver and Platinum, Lost Wax)</li>
+                <li>Hand-wrought designs (using laser technology)</li>
+              </ul>
+            </b-card-text>
+
+            <b-card-title>Custom Engraving Services</b-card-title>
+            <b-card-text>
+              <ul>
+                <li>By hand</li>
+                <li>By machine</li>
+                <li>Computer Generated</li>
+              </ul>
+            </b-card-text>
+
+            <b-card-title>Custom Designs and Engravings For:</b-card-title>
+            <b-card-text>
+              <ul>
+                <li>Signet Rings</li>
+                <li>Inside Rings</li>
+                <li>Family Crests</li>
+                <li>Glass (engraving)</li>
+                <li>Stone (engraving)</li>
+              </ul>
+            </b-card-text>
+
+            <b-card-title>Semi-Annual Inspection</b-card-title>
+            <b-card-text>
+              We recommend periodic cleaning and checking all of your jewelry to
+              keep it looking good, prevent losing stones and to advise you of
+              any repairs needed. If you notice any irregularities, like
+              catching in clothing, broken or missing stones, or bent shank,
+              then it's time to visit Aesthetics In Jewelry.
+            </b-card-text>
+          </b-tab>
+          <b-tab title="Replacements & Appraisals">
+            <b-card-title>Replacements</b-card-title>
+            <b-card-text>
+              <ul>
+                <li>Total Piece Replacement</li>
+                <li>Stone Replacement</li>
+              </ul>
+            </b-card-text>
+
+            <b-card-title>Appraisals</b-card-title>
+            <b-card-text>
+              <ul>
+                <li>Insurance</li>
+                <li>Estate</li>
+              </ul>
+            </b-card-text>
+
+            <b-card-title>Semi-Annual Inspection</b-card-title>
+            <b-card-text>
+              We recommend periodic cleaning and checking all of your jewelry to
+              keep it looking good, prevent losing stones and to advise you of
+              any repairs needed. If you notice any irregularities, like
+              catching in clothing, broken or missing stones, or bent shank,
+              then it's time to visit Aesthetics In Jewelry.
+            </b-card-text>
+
+            <b-card-title>Free Appraisals</b-card-title>
+            <b-card-text>
+              Included with some newly purchased items.
+            </b-card-text>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </article>
   </section>
 </template>
