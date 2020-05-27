25f6e40498330167d7995ab80e060a30c56f387d
Justin Chiou <chiou.kai@gmail.com>
2020-05-27 19:53:17 -0400

    use spaces, not tabs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,12 @@ import { sendEmail } from "./sendgrid";
 const CONTACT_FORM_TEMPLATE_ID: string = functions.config().sendgrid.template.contact;
 
 export const contactForm = functions.firestore.document("messages/{id}").onCreate(
-	async (data, context) => {
-		try {
-			const values = data.data();
-	  	await sendEmail(CONTACT_FORM_TEMPLATE_ID, "chiou.kai@gmail.com", values);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+  async (data, context) => {
+    try {
+      const values = data.data();
+      await sendEmail(CONTACT_FORM_TEMPLATE_ID, "chiou.kai@gmail.com", values);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 );
