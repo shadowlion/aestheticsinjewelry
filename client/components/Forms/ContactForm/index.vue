8f39da291251b7e13d547e9798ceee7321d405b1
Justin Chiou <chiou.kai@gmail.com>
2020-05-26 23:36:58 -0400

    notify client upon message submission

--- client/components/Forms/ContactForm/index.vue
+++ client/components/Forms/ContactForm/index.vue
@@ -1,51 +1,60 @@
 <template>
-  <b-form @submit.prevent="submitContactForm">
-    <b-form-group label="Name:" label-for="input-name">
-      <b-form-input id="input-name" v-model="form.name" />
-    </b-form-group>
+  <div>
+    <b-form v-if="!sent" @submit.prevent="submitContactForm">
+      <b-form-group label="Name:" label-for="input-name">
+        <b-form-input id="input-name" v-model="form.name" />
+      </b-form-group>
 
-    <label for="input-method">Preferred Contact Method:</label>
-    <b-input-group class="pb-3">
-      <b-input-group-prepend is-text>
-        <b-form-radio-group
-          v-model="form.method"
-          name="input-method"
-          :options="['Phone', 'Email']"
+      <label for="input-method">Preferred Contact Method:</label>
+      <b-input-group class="pb-3">
+        <b-input-group-prepend is-text>
+          <b-form-radio-group
+            v-model="form.method"
+            name="input-method"
+            :options="['Phone', 'Email']"
+          />
+        </b-input-group-prepend>
+        <b-form-input
+          id="input-method"
+          v-model="form.methodData"
+          :placeholder="
+            form.method === 'Phone' ? '(###) ###-####' : 'something@domain.com'
+          "
         />
-      </b-input-group-prepend>
-      <b-form-input
-        id="input-method"
-        v-model="form.methodData"
-        :placeholder="
-          form.method === 'Phone' ? '(###) ###-####' : 'something@domain.com'
-        "
-      />
-    </b-input-group>
+      </b-input-group>
 
-    <b-form-group label="Message:" label-for="input-message">
-      <b-form-textarea id="input-message" v-model="form.message" rows="3" />
-    </b-form-group>
+      <b-form-group label="Message:" label-for="input-message">
+        <b-form-textarea id="input-message" v-model="form.message" rows="3" />
+      </b-form-group>
 
-    <button class="btn btn-primary" type="submit">Submit</button>
-  </b-form>
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </b-form>
+
+    <p v-else>Sent!</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import firebase from "@/plugins/firebase";
+
 export default Vue.extend({
   data() {
     return {
       form: {
         name: null,
         method: null,
-        methodData: null
-      }
+        methodData: null,
+        message: null
+      },
+      sent: false
     };
   },
   methods: {
     submitContactForm() {
-      // eslint-disable-next-line
-      console.log("submitted!");
+      this.sent = true;
+      const contactForm = firebase.functions().httpsCallable("contactForm");
+      contactForm(this.form);
     }
   }
 });
