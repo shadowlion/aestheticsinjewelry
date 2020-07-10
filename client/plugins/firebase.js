fdc26827b1f817dcd379f1097aaca641d236cf25
Justin Chiou <chiou.kai@gmail.com>
2020-07-10 11:08:49 -0400

    Revert "change hours due to COVID-19"
    This reverts commit 563aac29f4d685a56a3a29555e44b2770d4e410d.

--- client/plugins/firebase.js
+++ client/plugins/firebase.js
@@ -3,6 +3,8 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   appId: process.env.FIREBASE_APP_ID
 };
