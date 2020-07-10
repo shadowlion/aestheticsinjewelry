563aac29f4d685a56a3a29555e44b2770d4e410d
Justin Chiou <chiou.kai@gmail.com>
2020-07-10 08:34:30 -0400

    change hours due to COVID-19

--- client/components/Base/BaseFooter/index.vue
+++ client/components/Base/BaseFooter/index.vue
@@ -4,6 +4,7 @@
       <div class="row d-flex justify-content-between">
         <div class="col-md-6">
           <h2>Come Visit Us!</h2>
+
           <a
             href="https://www.google.com/maps/place/410+W+Chestnut+St,+Louisville,+KY+40202/@38.2488751,-85.7606713,17z/data=!3m1!4b1!4m5!3m4!1s0x886972afaf855dd3:0x7fda89111b43c510!8m2!3d38.2488751!4d-85.7584826"
             target="_blank"
@@ -18,14 +19,19 @@
           </a>
 
           <address>
-            M-F: 10:00am - 5:30pm
+            M-F: 10:00am - 4:00pm
             <br />
-            Sat: 11:00am - 3:00pm
+            Sat: 10:00am - 3:00pm
             <br />
             Sun: Closed
           </address>
 
           <address>
+            <p class="text-muted mb-0">
+              Contact us ahead of time for an appointment so we can give you
+              privacy/social distancing:
+            </p>
+            <br />
             Phone: <a href="tel:5025892728">(502)589-2728</a>
             <br />
             Email:
--- client/plugins/firebase.js
+++ client/plugins/firebase.js
@@ -3,8 +3,6 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   appId: process.env.FIREBASE_APP_ID
 };
