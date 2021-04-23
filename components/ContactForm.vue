30616187973c852df77558d1b6e69785dfca1837
Justin Chiou <chiou.kai@gmail.com>
2021-04-23 13:52:24 -0400

    fix: remove old ajax state

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,46 +1,36 @@
 <template>
-  <div>
-    <form
-      v-if="!sent"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="contact_us" value="contact_us" />
+  <form method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="contact_us" value="contact_us" />
 
-      <b-form-group label="Name:" label-for="input-name">
-        <b-form-input id="input-name" name="name" required />
-      </b-form-group>
+    <b-form-group label="Name:" label-for="input-name">
+      <b-form-input id="input-name" name="name" required />
+    </b-form-group>
 
-      <label for="input-method">Preferred Contact Method:</label>
-      <b-input-group class="pb-3">
-        <b-input-group-prepend is-text>
-          <b-form-radio-group
-            id="input-method-type"
-            name="method"
-            :options="['Phone', 'Email']"
-            required
-          />
-        </b-input-group-prepend>
-        <b-form-input
-          id="input-method"
-          name="methodData"
-          :placeholder="methodPlaceholder"
+    <label for="input-method">Preferred Contact Method:</label>
+    <b-input-group class="pb-3">
+      <b-input-group-prepend is-text>
+        <b-form-radio-group
+          id="input-method-type"
+          v-model="method"
+          name="method"
+          :options="['Phone', 'Email']"
           required
         />
-      </b-input-group>
+      </b-input-group-prepend>
+      <b-form-input
+        id="input-method"
+        name="methodData"
+        :placeholder="methodPlaceholder"
+        required
+      />
+    </b-input-group>
 
-      <b-form-group label="Message:" label-for="input-message">
-        <b-form-textarea id="input-message" name="message" rows="3" required />
-      </b-form-group>
+    <b-form-group label="Message:" label-for="input-message">
+      <b-form-textarea id="input-message" name="message" rows="3" required />
+    </b-form-group>
 
-      <button class="btn btn-primary" type="submit">Submit</button>
-    </form>
-
-    <p v-else>
-      Thanks for your submission! We'll get back to you as soon as possible.
-    </p>
-  </div>
+    <button class="btn btn-primary" type="submit">Submit</button>
+  </form>
 </template>
 
 <script lang="ts">
@@ -49,18 +39,12 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      form: {
-        name: null,
-        method: null,
-        methodData: null,
-        message: null,
-      },
-      sent: false,
+      method: null,
     };
   },
   computed: {
     methodPlaceholder() {
-      switch (this.form.method as string | null) {
+      switch (this.method as string | null) {
         case "Phone":
           return "Please enter a phone number";
         case "Email":
@@ -70,13 +54,5 @@ export default Vue.extend({
       }
     },
   },
-  // methods: {
-  //   submitContactForm(e: Event) {
-  //     this.sent = true;
-  //     const { name, method, methodData, message } = Object.fromEntries(
-  //       new FormData(e.target)
-  //     );
-  //   },
-  // },
 });
 </script>
