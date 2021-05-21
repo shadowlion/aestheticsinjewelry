59f0649254ca8463b8515fdc1eac0f1fccaea842
Justin Chiou <chiou.kai@gmail.com>
2021-05-21 08:20:15 -0700

    fix: add social media back in

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,23 @@
 <template>
   <header class="container">
+    <div class="d-none d-md-block">
+      <b-navbar toggleable="md" type="light" variant="transparent">
+        <span class="navbar-text">Let's find you some amazing jewelry!</span>
+        <b-collapse is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="mailto:info@aestheticsinjewelry.com">
+              Email
+            </b-nav-item>
+            <b-nav-item href="tel:5025892728">Phone</b-nav-item>
+            <b-nav-item href="/visit-us">Youtube</b-nav-item>
+            <b-nav-item href="">Facebook</b-nav-item>
+            <b-nav-item href="/services">Instagram</b-nav-item>
+            <b-nav-item href="">Our Business Page</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+      <hr class="my-0" />
+    </div>
     <b-navbar toggleable="md" type="light" variant="transparent">
       <b-navbar-brand href="/">
         <b-img :src="require('@/assets/aj-logo.png')" width="200" />
