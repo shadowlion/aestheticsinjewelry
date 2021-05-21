b1c8ebf4621b93d3ac47897522bc4cdb3d3d97a6
Justin Chiou <chiou.kai@gmail.com>
2021-05-21 07:12:17 -0700

    refactor: modulate layout components

---
+++ components/MobileHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="d-block d-md-none">
+    <BNavbar toggleable="md" class="pb-md-4">
+      <BNavbarBrand href="#">Aesthetics in Jewelry</BNavbarBrand>
+      <BNavbarToggle target="nav-collapse"></BNavbarToggle>
+      <BCollapse id="nav-collapse" is-nav>
+        <BNavbarNav>
+          <BNavItem to="/">Home</BNavItem>
+          <BNavItem to="/about">About Us</BNavItem>
+          <BNavItem to="/exceptionals">The Exceptionals</BNavItem>
+          <BNavItem to="/birthstones">Birthstones</BNavItem>
+          <BNavItem to="/services">Services</BNavItem>
+          <BNavItem to="/visit-us">Visit Us</BNavItem>
+          <BNavItem href="tel:5025892728">(502) 589-2728</BNavItem>
+          <BNavItem href="mailto:info@aestheticsinjewelry.com">
+            info@aestheticsinjewelry.com
+          </BNavItem>
+          <BNavItem
+            href="https://www.youtube.com/channel/UCnsJN9wV4YbptB8NmlXFVaQ"
+            target="_blank"
+          >
+            Youtube
+          </BNavItem>
+          <BNavItem
+            href="https://www.facebook.com/aestheticsinjewelry/"
+            target="_blank"
+          >
+            Facebook
+          </BNavItem>
+          <BNavItem
+            href="https://www.instagram.com/aestheticsinjewel/"
+            target="_blank"
+          >
+            Instagram
+          </BNavItem>
+        </BNavbarNav>
+      </BCollapse>
+    </BNavbar>
+  </header>
+</template>
