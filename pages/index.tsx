93783125ced8ed6519d393941d0a504325a11bd1
Justin Chiou <chiou.kai@gmail.com>
2021-05-30 11:46:08 -0700

    refactor: add carousel images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from "next/head";
+import Carousel from "react-bootstrap/Carousel";
 
 const HomePage = () => {
   return (
     <>
       <Head>
-        <title>Home | Ballroom East</title>
+        <title>Home | Aesthetics in Jewelry</title>
         <meta name="description" content="Ballroom East Dance Studio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <article id="home">
-        <h1>Homepage</h1>
+      <article id="home" className="container pb-5 pt-3">
+        <Carousel>
+          {["1.jpg", "2.png", "3.png"].map((el) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={`/carousel-${el}`}
+                alt={`Image: carousel-${el}`}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </article>
     </>
   );
