2bd6b217e2ccaf52a293f0b490ab342f4eda86ce
Justin Chiou <chiou.kai@gmail.com>
2021-05-30 21:43:04 -0700

    feat: add gAnalytics back in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 import Layout from "../components/Layout";
 
+const NEXT_PUBLIC_GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    ReactGA.initialize(NEXT_PUBLIC_GOOGLE_ANALYTICS);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
+
   return (
     <Layout>
       <Component {...pageProps} />
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+const NEXT_PUBLIC_GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}"
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
