56a4c49ca3f5bd0f31e9c02f5765238cce9b110c
Justin Chiou <chiou.kai@gmail.com>
2021-05-30 19:49:43 -0700

    refactor: use sendgrid to test emailing

--- pages/api/twilio.ts
+++ pages/api/twilio.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
 
 type RequestBody = {
   name: string;
@@ -7,10 +8,37 @@ type RequestBody = {
   message: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
-    const data: RequestBody = req.body;
-    res.status(200).json({ message: "OK" });
+    try {
+      const data: RequestBody = req.body;
+      const { name, preferredMethod, methodInfo, message } = data;
+
+      const emailConfig: MailDataRequired = {
+        to: process.env.SENDGRID_TO_EMAIL,
+        from: process.env.SENDGRID_FROM_EMAIL,
+        subject: "Aesthetics in Jewelry: New Message",
+        text: "This goes away!",
+        templateId: process.env.SENDGRID_TEMPLATE_ID,
+        dynamicTemplateData: {
+          name,
+          preferredMethod,
+          methodInfo,
+          message,
+        },
+      };
+
+      await sgMail.send(emailConfig);
+
+      res.status(200).json({ message: "OK" });
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
   } else {
     res.status(405).json({ message: "Only POST request allowed." });
   }
