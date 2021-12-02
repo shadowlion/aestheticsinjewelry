496c63c1e381fd89f7f31bee36354f78aa85699c
Justin Chiou <chiou.kai@gmail.com>
2021-12-02 09:26:45 -0500

    fix: remove react-bootstrap from exceptionals page

--- pages/exceptionals/index.tsx
+++ pages/exceptionals/index.tsx
@@ -1,15 +1,39 @@
 import Head from "next/head";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import Image from "next/image";
+import Exceptional1Img from "../../public/exceptional-1.png";
+import Exceptional2Img from "../../public/exceptional-2.png";
+import Exceptional3Img from "../../public/exceptional-3.png";
+import Exceptional4Img from "../../public/exceptional-4.png";
+import Exceptional5Img from "../../public/exceptional-5.png";
+import Exceptional6Img from "../../public/exceptional-6.png";
 
-const exceptionals = [
-  { title: "Large Open Sapphire and Diamond Ring" },
-  { title: "Emerald and Diamond Necklace" },
-  { title: "Yellow and White Diamond Ring" },
-  { title: "Round Sapphire and Diamond Ring" },
-  { title: "Emerald and Diamond Ring" },
-  { title: "Tri-Color Gold and Diamond Ring" },
+type Exceptional = { title: string; img: StaticImageData };
+
+const exceptionals: Exceptional[] = [
+  {
+    title: "Large Open Sapphire and Diamond Ring",
+    img: Exceptional1Img,
+  },
+  { title: "Emerald and Diamond Necklace", img: Exceptional2Img },
+  { title: "Yellow and White Diamond Ring", img: Exceptional3Img },
+  { title: "Round Sapphire and Diamond Ring", img: Exceptional4Img },
+  { title: "Emerald and Diamond Ring", img: Exceptional5Img },
+  { title: "Tri-Color Gold and Diamond Ring", img: Exceptional6Img },
 ];
+
+const ExceptionalCard = ({ exceptional }: { exceptional: Exceptional }) => {
+  return (
+    <div className="card">
+      <div className="card-img-top text-center">
+        <Image src={exceptional.img} alt={exceptional.title} />
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{exceptional.title}</h5>
+        <button className="btn btn-primary stretched-link">Details</button>
+      </div>
+    </div>
+  );
+};
 
 const ExceptionalsPage = () => {
   return (
@@ -36,21 +60,7 @@ const ExceptionalsPage = () => {
             {exceptionals.map((exceptional, index) => {
               return (
                 <div className="col-12 col-md-4 mb-3" key={index}>
-                  <Card>
-                    <Card.Img
-                      src={`/exceptional-${index + 1}.png`}
-                      alt={exceptional.title}
-                    />
-                    <Card.Body>
-                      <Card.Title>{exceptional.title}</Card.Title>
-                      <Button
-                        href={`/exceptionals/${index + 1}`}
-                        className="stretched-link"
-                      >
-                        Details
-                      </Button>
-                    </Card.Body>
-                  </Card>
+                  <ExceptionalCard exceptional={exceptional} />
                 </div>
               );
             })}
