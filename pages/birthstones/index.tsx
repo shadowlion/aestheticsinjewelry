deaa676dc99c5e5f6b5423bdaf537e97daa758b5
Justin Chiou <chiou.kai@gmail.com>
2021-12-02 09:44:36 -0500

    fix: remove react-bootstrap from birthstones page

--- pages/birthstones/index.tsx
+++ pages/birthstones/index.tsx
@@ -1,7 +1,31 @@
 import Head from "next/head";
-import Card from "react-bootstrap/Card";
-import { birthstones } from "../../data";
+import Image from "next/image";
+import Link from "next/link";
+import { birthstones, Birthstone } from "../../data";
 import { getMonthFromIndex } from "../../helpers";
+
+const BirthstoneCard = ({
+  birthstone,
+  index,
+}: {
+  birthstone: Birthstone;
+  index: number;
+}) => {
+  return (
+    <div className="card text-center">
+      <div className="card-img-top">
+        <Image src={birthstone.img} alt={birthstone.title} />
+      </div>
+      <div className="card-body">
+        <h5 className="card-title pt-3">{getMonthFromIndex(index)}</h5>
+        <h6 className="card-subtitle text-muted pb-3">({birthstone.title})</h6>
+        <Link href={`/birthstones/${index + 1}`}>
+          <a className="stretched-link">Details</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 const BirthstonesPage = () => {
   return (
@@ -20,23 +44,7 @@ const BirthstonesPage = () => {
           <div className="row">
             {birthstones.map((birthstone, index) => (
               <div className="col-12 col-md-4 col-lg-2 mb-3" key={index}>
-                <Card className="text-center">
-                  <Card.Img src={`/stone-${index + 1}.jpg`} alt={birthstone} />
-                  <Card.Title className="pt-3">
-                    {getMonthFromIndex(index)}
-                  </Card.Title>
-                  <Card.Subtitle className="text-muted pb-3">
-                    ({birthstone})
-                  </Card.Subtitle>
-                  <Card.Footer>
-                    <Card.Link
-                      href={`/birthstones/${index + 1}`}
-                      className="stretched-link"
-                    >
-                      Details
-                    </Card.Link>
-                  </Card.Footer>
-                </Card>
+                <BirthstoneCard birthstone={birthstone} index={index} />
               </div>
             ))}
           </div>
