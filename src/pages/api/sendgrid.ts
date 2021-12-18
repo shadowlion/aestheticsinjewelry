c546a1df2e2ae0d6e7d6628fe89f40af1376af98
Justin Chiou <chiou.kai@gmail.com>
2021-12-18 11:21:32 -0500

    refactor: redo api page route

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -5,15 +5,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    const templateId = process.env.SENDGRID_TEMPLATE_ID;
-    const payload: RequestBody = JSON.parse(req.body);
-    const [, error] = await sendEmail(templateId, payload);
-    if (error) {
-      return res.status(400).json({ message: error });
-    }
-    return res.status(200).json({ message: "OK" });
-  } else {
+  if (req.method !== "POST") {
     return res.status(405).json({ message: "Only POST request allowed." });
   }
+
+  const templateId = process.env.SENDGRID_TEMPLATE_ID;
+  const payload: RequestBody = JSON.parse(req.body);
+  const [, error] = await sendEmail(templateId, payload);
+
+  if (error) {
+    return res.status(400).json({ message: error });
+  }
+
+  return res.status(200).json({ message: "OK" });
 }
